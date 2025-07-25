7c80377f3d453f49c3f128da630da2e16521b05d
drkgrkn <dorukgurkan1@gmail.com>
2025-07-25 16:56:17 +0300

    fixed tabwidth

--- init.lua
+++ init.lua
@@ -121,6 +121,7 @@ end)
 -- Enable break indent
 vim.o.breakindent = true
 vim.o.shiftwidth = 4
+vim.o.tabstop = 4
 vim.o.expandtab = false
 
 -- Save undo history
@@ -933,12 +934,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    'ellisonleao/gruvbox.nvim',
+    --    'folke/tokyonight.nvim',
+    --'ellisonleao/gruvbox.nvim',
+    'Mofiqul/dracula.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('dracula').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -947,7 +949,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'dracula'
     end,
   },
 
